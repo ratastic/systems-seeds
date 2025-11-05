f586676490b2411f4362f4fb4ca8508ac489d813
vega jafri <giantwhitedeer@gmail.com>
2025-11-04 22:58:06 -0500

    project made; cube movement

---
+++ seed-prototypes/Assets/Scenes/seed01/CubeMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CubeMovement : MonoBehaviour
+{
+    [SerializeField] private float rollSpeed = 3f;
+    private bool isMoving;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving) return;
+
+        if (Input.GetKey(KeyCode.W)) Assemble(Vector3.forward);
+        if (Input.GetKey(KeyCode.A)) Assemble(Vector3.left);
+        if (Input.GetKey(KeyCode.S)) Assemble(Vector3.back);
+        if (Input.GetKey(KeyCode.D)) Assemble(Vector3.right);
+        
+        void Assemble(Vector3 dir)
+        {
+            var anchor = transform.position + (Vector3.down + dir) * .5f;
+            var axis = Vector3.Cross(Vector3.up, dir);
+            StartCoroutine(Roll(anchor, axis));
+        }
+    }
+
+    IEnumerator Roll(Vector3 anchor, Vector3 axis)
+    {
+        isMoving = true;
+
+        for (int i = 0; i < (90 / rollSpeed); i++)
+        {
+            transform.RotateAround(anchor, axis, rollSpeed);
+            yield return new WaitForSeconds(.01f);
+        }
+
+        isMoving = false;
+    }
+}
