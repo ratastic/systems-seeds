1d5012536a654c9bcbe8b958be4a27e0e9218005
vega jafri <giantwhitedeer@gmail.com>
2025-11-05 01:33:05 -0500

    asdfsgh

--- seed-prototypes/Assets/Scenes/seed01/CylinderMovement.cs
+++ seed-prototypes/Assets/Scenes/seed01/CylinderMovement.cs
@@ -13,24 +13,23 @@ public class CylinderMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         cylinderCanRoll = false;
-        if (cylinderCanRoll == true)
-        {
-            FixedUpdate();
-        }
     }
 
     public void FixedUpdate()
     {
-        float upDown = Input.GetAxis("Vertical");
-        float leftRight = Input.GetAxis("Horizontal");
+        if (cylinderCanRoll == true)
+        {
+            float upDown = Input.GetAxis("Vertical");
+            float leftRight = Input.GetAxis("Horizontal");
 
-        Vector3 movement = new Vector3(0, 0, upDown * moveSpeed);
-        Vector3 tilt = new Vector3(0, 0, leftRight * rotationTilt);
+            Vector3 movement = new Vector3(0, 0, upDown * moveSpeed);
+            Vector3 tilt = new Vector3(0, 0, leftRight * rotationTilt);
 
-        rb.AddForce(movement);
+            rb.AddForce(movement);
 
-        Quaternion playerRotationVar = Quaternion.Euler(-tilt * Time.deltaTime);
+            Quaternion playerRotationVar = Quaternion.Euler(-tilt * Time.deltaTime);
 
-        rb.MoveRotation(rb.rotation * playerRotationVar);
+            rb.MoveRotation(rb.rotation * playerRotationVar);
+        }
     }
 }
